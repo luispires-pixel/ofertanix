1e662a0e2b853c96988a227fb1423222063e2b27
Luis Henrique <luispire633@gmail.com>
2026-09-26 18:54:03 -0300

    Corrige armazenamento de imagens

--- config/settings.py
+++ config/settings.py
@@ -91,6 +91,9 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
