eaf650fd6ae704c46f9709cc0022e3ce98441cde
Luis Henrique <luispire633@gmail.com>
2026-09-26 19:14:48 -0300

    Corrige URLs de mídia

--- config/urls.py
+++ config/urls.py
@@ -3,5 +3,15 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from ofertas import views
-urlpatterns = [path("admin/", admin.site.urls), path("", views.home, name="home"), path("go/<int:pk>/", views.go, name="go")]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)git add config/urls.py
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.home, name="home"),
+    path("go/<int:pk>/", views.go, name="go"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
